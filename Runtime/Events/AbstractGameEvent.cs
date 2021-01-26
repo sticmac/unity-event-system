36347dca35c7de00fd3c6320c1f1c7310f90f117
Julien Lemaire <julien.lemaire@sticmac.fr>
2021-01-26 23:19:19 +0100

    Basic structure of the event system

---
+++ Runtime/Events/AbstractGameEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sticmac.EventSystem {
+    /// <summary>
+    /// Parent class for all game events
+    /// </summary>
+    /// <typeparam name="T">Listener associated with the event</typeparam>
+    public abstract class AbstractGameEvent<T> : ScriptableObject where T : AbstractListener
+    {
+        protected List<T> _listeners = new List<T>();
+
+        public void RegisterListener(T listener) {
+            _listeners.Add(listener);
+        }
+
+        public void UnregisterListener(T listener) {
+            _listeners.Remove(listener);
+        }
+    }
+}
---
+++ Runtime/Listener/AbstractListener.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Sticmac.EventSystem {
+    /// <summary>
+    /// Parent class for all game event listeners
+    /// </summary>
+    public abstract class AbstractListener : MonoBehaviour {}
+}
