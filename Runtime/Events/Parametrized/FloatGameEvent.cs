59a7c5faaccbcd52f3a8835ed0cadff296c29baa
Julien Lemaire <julien.lemaire@sticmac.fr>
2021-01-29 00:37:55 +0100

    Adding Float Game Event

---
+++ Runtime/Events/Parametrized/FloatGameEvent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sticmac.EventSystem {
+
+    [CreateAssetMenu(fileName = "Float Event", menuName = "Event System/Float Event", order = 1)]
+    public class FloatGameEvent : ParametrizedGameEvent<ParametrizedGameEventListener<float>, float> {}
+}
