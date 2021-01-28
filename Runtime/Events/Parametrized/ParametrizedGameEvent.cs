34d60b95f03db94c26d6a3aa6a65aed384ce2648
Julien Lemaire <julien.lemaire@sticmac.fr>
2021-01-28 22:25:19 +0100

    Stable structure for parametrized game events

--- Runtime/Events/Parametrized/ParametrizedGameEvent.cs
+++ Runtime/Events/Parametrized/ParametrizedGameEvent.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Sticmac.EventSystem {
-    public class ParametrizedGameEvent<T, U> : AbstractGameEvent<T> where T : ParametrizedGameEventListener<U> where U : struct
+    public class ParametrizedGameEvent<T, U> : AbstractGameEvent<T>
+        where T : ParametrizedGameEventListener<U>
+        where U : struct
     {
         public void Raise(U value) {
             for(int i = _listeners.Count -1; i >= 0; i--)
--- Runtime/Listener/Parametrized/ParametrizedGameEventListener.cs
+++ Runtime/Listener/Parametrized/ParametrizedGameEventListener.cs
@@ -4,9 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Sticmac.EventSystem {
-    public abstract class ParametrizedGameEventListener<T> : AbstractListener where T : struct
+    public abstract class ParametrizedGameEventListener<T> : AbstractListener
+        where T : struct
     {
+        [SerializeField] ParametrizedGameEvent<ParametrizedGameEventListener<T>, T> Event;
+
         public event Action<T> OnRaised;
+
+        private void OnEnable() {
+            Event.RegisterListener(this);
+        }
+
+        private void OnDisable() {
+            Event.UnregisterListener(this);
+        }
 
         public void OnEventRaised(T value) {
             OnRaised?.Invoke(value);
